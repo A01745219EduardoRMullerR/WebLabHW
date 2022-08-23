f5c7a7f1849f68ee1a3fff82c386c00808d78955
MullerDerRaumdeuter <a01745219@itesm.mx>
2022-08-22 21:16:49 -0500

    almost there

--- Practica2_JavaScript/ejercicio1.js
+++ Practica2_JavaScript/ejercicio1.js
@@ -5,22 +5,55 @@ class Calculadora{
     suma(a, b, c = 1){
         this.operacion = "suma";
         let result = (a + b) * c;
+        if(c === 1){
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + result);
+        }else{
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + this.suma(a, b) + " y multiplicado por "
+            + c + " es igual a " + result);
+        }
     }
     resta(a, b, c = 1){
         this.operacion = "resta";
         let result = (a - b) * c;
+        if(c === 1){
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + result);
+        }else{
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + this.resta(a, b) + " y multiplicado por "
+            + c + " es igual a " + result);
+        }
     }
     multiplicacion(a, b, c = 1){
         this.operacion = "multiplicacion"
         let result = a * b * c;
+        if(c === 1){
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + result);
+        }else{
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + this.multiplicacion(a, b) + " y multiplicado por "
+            + c + " es igual a " + result);
+        }
     }
     division(a, b, c = 1){
         this.operacion = "division";
         let result = a / b * c;
+        if(c === 1){
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + result);
+        }else{
+            console.log("La operacion " + this.operacion + " de " + a + " y " + b + " resulta en " + this.division(a, b) + " y multiplicado por "
+            + c + " es igual a " + result);
+        }
     }
 }
 
 function main(){
     let calc1 = new Calculadora();
-    
+    calc1.suma(8, 45);
+    calc1.suma(44, 487, 2);
+    calc1.resta(4444551, 4444550);
+    calc1.resta(12345, 12333, 5);
+    calc1.multiplicacion(1, 2, 3)
+    calc1.multiplicacion(5, 5);
+    calc1.division(45, 5);
+    calc1.division(144, 12, 2);
 }
+
+main()
