df9b8459dbd07c027f18e3bae6f4da74cbbb7f17
MullerDerRaumdeuter <a01745219@itesm.mx>
2022-09-03 16:26:04 -0500

    card component

--- ConsultaVideojuegos/consultavj/src/componentes/InfoVideojuegos.js
+++ ConsultaVideojuegos/consultavj/src/componentes/InfoVideojuegos.js
@@ -3,7 +3,12 @@ import React from "react";
 export const InfoVideojuegos = () =>{
     return(
         <>
-            <h2 className="Card-title">Info</h2>
+            <div className="card">
+                <h2 className="card_title">Info</h2>
+                <img src=""></img>
+                <h4 className="card_body"></h4>
+                <p className="card_description"></p>
+            </div>
         </>
     )
 }
