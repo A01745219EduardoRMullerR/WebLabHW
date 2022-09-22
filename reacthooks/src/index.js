a8432fadfbd9d34b475d3f1170d7a0c7921a0642
MullerDerRaumdeuter <a01745219@itesm.mx>
2022-09-22 18:04:01 -0500

    practica grupal hooks pt2

--- reacthooks/src/index.js
+++ reacthooks/src/index.js
@@ -1,13 +1,17 @@
  
 import React from 'react'; 
 import { createRoot } from 'react-dom/client'; 
-import { Hooks } from './Hooks'; 
-import { Formulario } from './hooks/effect/Formulario';
-import { Contador } from './hooks/state/Contador';
-import { ContadorConCustomHook } from './hooks/state/ContadorConCustomHook';
+import { HookUseMemo } from './hooks/memo/HookUseMemo';
+import { Memoriza } from './hooks/memo/Memoriza';
+//import { Hooks } from './Hooks'; 
+//import { Formulario } from './hooks/effect/Formulario';
+//import { FocusScreen } from './hooks/ref/FocusScreen';
+//import { Contador } from './hooks/state/Contador';
+//import { ContadorConCustomHook } from './hooks/state/ContadorConCustomHook';
+//import { MultipleCustomHooks } from './hooks/state/MultpleCustomHooks';
  
 const container = document.getElementById('root'); 
  
 const root = createRoot(container); 
  
-root.render(<Formulario />);
+root.render(<HookUseMemo />);
